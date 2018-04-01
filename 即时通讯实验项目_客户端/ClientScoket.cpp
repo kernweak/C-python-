aaf0cf4fe43a4a8f00a1eb75254e3b3fd7ef4a5c
51536 <51536@DESKTOP-2U4QH93>
2018-04-02 01:14:13 +0800

    123

--- 即时通讯实验项目_客户端/ClientScoket.cpp
+++ 即时通讯实验项目_客户端/ClientScoket.cpp
@@ -10,3 +10,47 @@ CClientScoket::CClientScoket()
 CClientScoket::~CClientScoket()
 {
 }
+
+bool CClientScoket::ConnectServer(char * szIp, WORD port)
+{
+	//1.初始化套接字动态库
+	WSADATA wsd;
+	if (WSAStartup(MAKEWORD(2, 2), &wsd) != 0) {
+		return false;
+	}
+	//检查返回的库是否是2.2
+	if (LOBYTE(wsd.wVersion) != 2 || HIBYTE(wsd.wHighVersion) != 2) {
+		WSACleanup();
+		return false;
+	}
+	//2.创建套接字
+	m_sClient = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (m_sClient == INVALID_SOCKET) {
+		WSACleanup();
+		return false;
+	}
+	//3.设置服务器地址
+	SOCKADDR_IN servAddr = {};//服务器地址
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = inet_addr(szIp);
+	servAddr.sin_port = htons((short)port);
+	//6.连接服务器
+	//SOCKERT sService;//跟服务端相连的套接字
+	int nRetValue = connect(m_sClient, (sockaddr*)&servAddr, sizeof(SOCKADDR_IN));
+	if (nRetValue == SOCKET_ERROR) {
+		closesocket(m_sClient);
+		m_sClient = NULL;
+		WSACleanup();
+		return false;
+	}
+	return true;
+}
+
+
+bool CClientScoket::Close()
+{
+	WSACleanup();
+	closesocket(m_sClient);
+	return true;
+}
+
--- 即时通讯实验项目_客户端/ClientScoket.h
+++ 即时通讯实验项目_客户端/ClientScoket.h
@@ -1,5 +1,79 @@
 #pragma once
 #include<vector>
+#define BUFMSG 2048//一次接收消息的最大字节数
+//头部：通讯目的，包括匿名登录，聊天1v1聊天，注册，登录，添加好友，搜索好友，文件传输，查看聊天记录
+enum CHATPURPOSE {
+	ANONYMOUS = 1,
+	CHAT,
+	ONE2ONE,
+	REGISTER,
+	LOGIN,
+	ADDFRIEND,
+	SEARCHUSER,
+	FILETRANS,
+	MSGRECORD,
+	UPDATEUSER
+};
+//聊天内容
+//匿名的结构体
+typedef struct _CHATANONYMOUS {
+	DWORD dwLen;//匿名名称长度
+	char buf[50];//匿名
+}CHATANONYMOUS;
+
+typedef struct _CHATCHAT {
+	DWORD dwLen;//聊天内容长度
+	char buf[1024];
+}CHATCHAT;
+
+//1v1结构体
+typedef struct _CHATONE2ONE {
+	CHAR szName[50];
+	CHAR szContent[1024];
+}CHATONE2ONE;
+
+//注册结构体
+typedef struct _CHATREGISTER {
+	CHAR szName[50];
+	CHAR szPwd[50];
+}CHATREGISTER;
+
+//登录结构体，服务器返回结果放在szName里
+typedef struct _CHATLOGIN {
+	CHAR szName[50];
+	CHAR szPwd[50];
+}CHATLOGIN;
+
+//添加好友结构体
+typedef struct _CHATADDFRIEND {
+	CHAR szName[50];//自己姓名
+	CHAR szFrd[50];//要添加好友姓名
+}CHATADDFRIEND;
+
+//搜索用户结构体
+typedef struct _CHATSEARCHUSER {
+	CHAR szName[50];//要搜索的用户名
+}CHATSEARCHUSER;
+
+//文件传输结构体
+typedef struct _CHATFILETRANS {
+
+}CHATFILETRANS;
+
+//查看聊天记录结构体
+typedef struct _CHATMSGRECORD {
+	CHAR szFrom[50];
+	CHAR szTo[50];
+	CHAR szContent[1024];
+}CHATMSGRECORD;
+
+//更新在线用户列表
+typedef struct _CHATUPDATEUSER {
+
+};
+
+//查看聊天
+
 class CClientScoket
 {
 public:
@@ -9,7 +83,7 @@ public:
 	bool ConnectServer(char* szIp, WORD port);
 	//负责创建线程用来接收服务器的信息
 	char* Recv();
-	bool Send(CHATPURPSE purpose, char*bufSend, DWORD dwLen);
+	bool Send(CHATPURPOSE purpose, char*bufSend, DWORD dwLen);
 	bool Close();
 private:
 #define BUFNAME 50
@@ -35,8 +109,8 @@ private:
 	char* SendForGetMsgRecord();
 public:
 	//保存聊天记录使用
-	HANDLE m_hEvent = 0;
-	vector<CHATMSGRECORD> m_vecMsgRecord;
+		HANDLE m_hEvent = 0;
+vector<CHATMSGRECORD> m_vecMsgRecord;
 	//客户端套接字
 	SOCKET m_sClient = NULL;
 	//新用户加入或退出聊天室用到
@@ -47,6 +121,6 @@ public:
 	char m_szName[BUFNAME] = {};
 	CHATSEND *m_pObjChatRecv;
 private:
-
+                                                                                                                                                              
 };
 
